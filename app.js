07c5be732551dee8459316630d4004cb49e8929b
Sergio <sergiadn335@gmail.com>
2023-03-21 17:40:34 +0100

    Primera Subida

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+app.use(cors())
+const port = 3000
+
+app.listen(port, () => {
+    console.log('Eyyy tu app corre por el puerto ' + port);
+})
+
+app.get("/", (req, res) => {
+    res.send("Hola Mundo/Hello World")
+})
